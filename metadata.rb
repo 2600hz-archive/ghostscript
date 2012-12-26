2aaca9ec0744ac351ac1856ad838388232aa2d62
Steve Lum <steve.lum@gmail.com>
2012-12-26 15:10:48 -0800

    first commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "2600hz"
+maintainer_email "stephen@2600hz.com"
+license          "Apache 2.0"
+description      "Installs/Configures ghostscript"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
